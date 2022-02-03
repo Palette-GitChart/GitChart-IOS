20858996e6c8ecf1e9ab84f14e6bee3cb70fec35
김대희 <0824dh@naver.com>
2022-02-03 22:52:48 +0900

    File :: 파일 정리

---
+++ GitChart/Sources/SceneDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  SceneDelegate.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/01/29.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+//        let mainViewController = ViewController() // 맨 처음 보여줄 ViewController
+//        let navigationController = UINavigationController(rootViewController: mainViewController)
+//        window?.rootViewController = navigationController
+//        window?.makeKeyAndVisible()
+//        guard let _ = (scene as? UIWindowScene) else { return }
+        
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+    
+    
+}
+
