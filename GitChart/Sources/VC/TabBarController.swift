3fab24201ca0ddfa9994bd3354e205826f898b7b
김대희 <0824dh@naver.com>
2022-02-08 23:41:31 +0900

    Add :: TabBarController

---
+++ GitChart/Sources/VC/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/02/07.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+}
