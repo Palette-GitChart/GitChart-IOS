be6bdaa7bbd2f27e97c09fe66b862648d98e218c
김대희 <0824dh@naver.com>
2022-02-08 23:41:07 +0900

    Add :: HimeVC

---
+++ GitChart/Sources/VC/HomeVC.swift
@@ -0,0 +1,14 @@
+//
+//  HomeVC.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/02/07.
+//
+
+import Foundation
+
+class HomeVC : BaseViewController {
+    override func configureUI() {
+        self.title = "GitChart"
+    }
+}
