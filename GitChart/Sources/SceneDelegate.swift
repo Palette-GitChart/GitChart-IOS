57380b337c2d3826d3f797abff00ed9d144c7a11
김대희 <0824dh@naver.com>
2022-02-08 23:42:03 +0900

    File ::  AssetsColor

--- GitChart/Sources/SceneDelegate.swift
+++ GitChart/Sources/SceneDelegate.swift
@@ -12,13 +12,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
-//        let mainViewController = ViewController() // 맨 처음 보여줄 ViewController
+//
+//        let mainViewController = HomeVC() // 맨 처음 보여줄 ViewController
 //        let navigationController = UINavigationController(rootViewController: mainViewController)
 //        window?.rootViewController = navigationController
 //        window?.makeKeyAndVisible()
 //        guard let _ = (scene as? UIWindowScene) else { return }
-        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정해줌
+        let mainViewController = HomeVC() // 맨 처음 보여줄 ViewController
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        guard let _ = (scene as? UIWindowScene) else { return }
+
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
