68463cc9012eba3cb484e749361f83ff4986cab9
김대희 <0824dh@naver.com>
2022-02-10 01:09:24 +0900

    Add :: Fonts

---
+++ GitChart/Resources/Extension/Font.swift
@@ -0,0 +1,18 @@
+//
+//  Font.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/02/10.
+//
+
+import Foundation
+
+enum AssetsFont {
+    case NotoSans_Regular
+    case NotoSans_Bold
+    
+    case SF_Pro_Rounded_Bold
+    case SF_Pro_Rounded_Semibold
+    case SF_Pro_Rounded_Medium
+    case SF_Pro_Rounded_Regular
+}
