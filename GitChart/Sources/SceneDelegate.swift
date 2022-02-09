b095e1bb1fc777127744ee53f3b83fc742f84905
김대희 <0824dh@naver.com>
2022-02-10 00:48:14 +0900

    Modify :: Scene rootViewController TabBarController

--- GitChart/Sources/SceneDelegate.swift
+++ GitChart/Sources/SceneDelegate.swift
@@ -12,20 +12,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-//
-//        let mainViewController = HomeVC() // 맨 처음 보여줄 ViewController
-//        let navigationController = UINavigationController(rootViewController: mainViewController)
-//        window?.rootViewController = navigationController
-//        window?.makeKeyAndVisible()
-//        guard let _ = (scene as? UIWindowScene) else { return }
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정해줌
-        let mainViewController = HomeVC() // 맨 처음 보여줄 ViewController
-        let navigationController = UINavigationController(rootViewController: mainViewController)
+        let mainViewController = TabBarController() // 맨 처음 보여줄 ViewController
 
-        window?.rootViewController = navigationController
+        window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
         guard let _ = (scene as? UIWindowScene) else { return }
+
 
     }
     
