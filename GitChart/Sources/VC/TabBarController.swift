f5083693d6abf6c88442db7c3cfda8113c4cf6d0
김대희 <0824dh@naver.com>
2022-02-09 23:39:13 +0900

    Add :: Tab Item

--- GitChart/Sources/VC/TabBarController.swift
+++ GitChart/Sources/VC/TabBarController.swift
@@ -17,5 +17,26 @@ class TabBarController: UITabBarController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        let tabOne = UINavigationController(rootViewController: MainHomeViewController())
+        let tabOneBarItem = UITabBarItem(title: "홈", image: UIImage(named: "baseline_home_black"), tag: 1)
+        
+        tabOne.tabBarItem = tabOneBarItem
+        
+        let tabTwo = UINavigationController(rootViewController: SendMoneyViewController())
+        let tabTwoBarItem2 = UITabBarItem(title: "송금", image: UIImage(named: "baseline_paid_black"), tag: 2)
+        
+        tabTwo.tabBarItem = tabTwoBarItem2
+        
+        let tabThr = UINavigationController(rootViewController: StockUIViewController())
+        let tabThrBarItem3 = UITabBarItem(title: "주식", image: UIImage(named: "baseline_trending_up_black"), tag: 3)
+        
+        tabThr.tabBarItem = tabThrBarItem3
+        
+        let tabFou = UINavigationController(rootViewController: OtherViewController())
+        let tabFouBarItem4 = UITabBarItem(title: "더보기", image: UIImage(named: "round_subject_black"), tag: 4)
+        
+        tabFou.tabBarItem = tabFouBarItem4
+        
+
     }
 }
