7c92c6ffae46fc4901f1ee83da2404a2ca3533bc
김대희 <0824dh@naver.com>
2022-02-10 11:18:52 +0900

    Add :: Fontkit

---
+++ GitChart/Resources/Extension/Fontkit.swift
@@ -0,0 +1,44 @@
+//
+//  Fontkit.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/02/10.
+//
+
+import Foundation
+import UIKit
+
+class FontKit {
+    
+    static func roundedFont(ofSize fontSize: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let systemFont = UIFont.systemFont(ofSize: fontSize, weight: weight)
+        let font: UIFont
+        
+        if #available(iOS 13.0, *) {
+            if let descriptor = systemFont.fontDescriptor.withDesign(.rounded) {
+                font = UIFont(descriptor: descriptor, size: fontSize)
+            } else {
+                font = systemFont
+            }
+        } else {
+            font = systemFont
+        }
+        return font
+    }
+}
+
+enum notoSans {
+    case Regular
+    case Bold
+}
+
+extension UIFont {
+    static func notoFont(size : notoSans, ofSize : CGFloat) -> UIFont {
+        switch size {
+        case .Regular:
+            return UIFont(name: "NotoSans", size: ofSize)!
+        case .Bold:
+            return UIFont(name: "NotoSans-Bold", size: ofSize)!
+        }
+    }
+}
