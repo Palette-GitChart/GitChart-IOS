fd6d87f2b42190f3a234fbcc5de523e773492652
김대희 <0824dh@naver.com>
2022-02-11 23:41:42 +0900

    File :: Rename ApiKey

---
+++ GitChart/Sources/Networking/BaseURL.swift
@@ -0,0 +1,13 @@
+//
+//  BaseURL.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/02/11.
+//
+
+import Foundation
+
+let gitURL = "https://api.github.com/users"
+
+let myURL = "http://3.38.95.103:5000"
+
