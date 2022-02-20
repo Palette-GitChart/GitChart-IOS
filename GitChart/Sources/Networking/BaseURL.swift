04bc54785da4e8e67d0b1fc4d8364ec66f6a6edd
김대희 <0824dh@naver.com>
2022-02-20 23:11:32 +0900

    Add :: getBaseURL func

--- GitChart/Sources/Networking/BaseURL.swift
+++ GitChart/Sources/Networking/BaseURL.swift
@@ -10,3 +10,21 @@ import Foundation
 let gitURL = "https://api.github.com/users"
 
 let myURL = "http://3.38.95.103:5000"
+
+extension API {
+    func getBaseURL() -> URL {
+        switch self {
+            
+        case .dayCommit(_), .weekCommit(_), .mounthCommit(_), .yearCommit(_):
+            return URL(string: myURL)!
+            
+        case .weekArray(_), .mounthArray(_), .yearArray(_):
+            return URL(string: myURL)!
+            
+        case .getUserProfile(_), .getUserStarred(_):
+            return URL(string: gitURL)!
+            
+        }
+    }
+    
+}
