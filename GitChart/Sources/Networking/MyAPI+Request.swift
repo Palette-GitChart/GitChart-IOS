5efde7e9b247514123be861065f765431caca19d
김대희 <0824dh@naver.com>
2022-02-20 23:05:14 +0900

    Add :: Moya Request

--- GitChart/Sources/Networking/MyAPI+Request.swift
+++ GitChart/Sources/Networking/MyAPI+Request.swift
@@ -120,3 +120,65 @@ extension API {
   }
 }
 
+// MARK: Moya Request
+extension API {
+  static let moya = MoyaWrapper.provider
+  
+  static var jsonDecoder: JSONDecoder {
+    let decoder = JSONDecoder()
+    return decoder
+  }
+  
+  func request(
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line
+  ) -> Single<Response> {
+    
+    let endpoint = API.Wrapper(base: self)
+    let requestString = "\(endpoint.method) \(endpoint.baseURL) \(endpoint.path)"
+
+    return Self.moya.rx.request(endpoint)
+      .filterSuccessfulStatusCodes()
+      .catch(self.handleInternetConnection)
+      .catch(self.handleTimeOut)
+      .catch(self.handleREST)
+      .do(
+        onSuccess: { response in
+          let requestContent = "🛰 SUCCESS: \(requestString) (\(response.statusCode))"
+          print(requestContent, file, function, line)
+        },
+        onError: { rawError in
+          switch rawError {
+          case MyAPIError.requestTimeout:
+            print("---TODO: alert MyAPIError.requestTimeout---")
+          case MyAPIError.internetConnection:
+            print("--TODO: alert MyAPIError.internetConnection---")
+          case let MyAPIError.restError(error, _, _):
+            guard let response = (error as? MoyaError)?.response else { break }
+            if let jsonObject = try? response.mapJSON(failsOnEmptyData: false) {
+              let errorDictionary = jsonObject as? [String: Any]
+              guard let key = errorDictionary?.first?.key else { return }
+              let message: String
+              if let description = errorDictionary?[key] as? String {
+                message = "🛰 FAILURE: \(requestString) (\(response.statusCode)\n\(key): \(description)"
+              } else if let description = (errorDictionary?[key] as? [String]) {
+                message = "🛰 FAILURE: \(requestString) (\(response.statusCode))\n\(key): \(description)"
+              } else if let rawString = String(data: response.data, encoding: .utf8) {
+                message = "🛰 FAILURE: \(requestString) (\(response.statusCode))\n\(rawString)"
+              } else {
+                message = "🛰 FAILURE: \(requestString) (\(response.statusCode)"
+              }
+              print(message)
+            }
+          default:
+            break
+          }
+        },
+        onSubscribe: {
+          let message = "REQUEST: \(requestString)"
+          print(message, file, function, line)
+        }
+      )
+  }
+}
