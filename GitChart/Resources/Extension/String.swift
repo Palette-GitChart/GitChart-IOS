72b0e01c01bb512ad468e0b2a50027c3c1e70bcc
김대희 <0824dh@naver.com>
2022-03-02 16:33:03 +0900

    Add :: localized func with argument

--- GitChart/Resources/Extension/String.swift
+++ GitChart/Resources/Extension/String.swift
@@ -12,6 +12,8 @@ extension String {
     func localized(comment: String = "") -> String {
         return NSLocalizedString(self, comment: comment)
     }
-    
+    func localized(with argument: CVarArg = [], comment: String = "") -> String {
+            return String(format: self.localized(comment: comment), argument)
+        }
 }
  
