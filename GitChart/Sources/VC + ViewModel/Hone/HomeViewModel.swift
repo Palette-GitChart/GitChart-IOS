be417cfa719a03dfe1ffe58c5da5ced3f480cca8
김대희 <0824dh@naver.com>
2022-03-02 16:53:25 +0900

    Add :: HomeViewModel getUserProfile request

--- GitChart/Sources/VC + ViewModel/Hone/HomeViewModel.swift
+++ GitChart/Sources/VC + ViewModel/Hone/HomeViewModel.swift
@@ -29,12 +29,36 @@ class HomeViewModel : ViewModel  {
     }
     
     func trans(_ input: input) -> output {
+        
+        
         let getUserProfile = PublishRelay<UserProfile>()
         let getUserDayCommit = PublishRelay<String>()
         let getWeekCommit = PublishRelay<String>()
         let getYearCommit = PublishRelay<String>()
         let getYearArray = PublishRelay<[Int]>()
         let usernameStatus = PublishRelay<Bool>()
+        
+        API.getUserProfile("kimdaehee0824").request()
+            .subscribe { (event) in
+                switch event {
+                case .success(let response):
+                    print(response.data)
+                    guard let data = try? JSONDecoder().decode(UserProfile.self, from: response.data) else {
+                        return
+                    }
+                    getUserProfile.accept(data)
+                    usernameStatus.accept(true)
+                case .failure(let error):
+                    print(error)
+                    usernameStatus.accept(true)
+                }
+                
+            }
+        
+        
+        
+        
+        
         return output(getUserProfile: getUserProfile, getUserDayCommit: getUserDayCommit, getWeekCommit: getWeekCommit, getYearCommit: getYearCommit, getYearArray: getYearArray, usernameStatus: usernameStatus)
     }
     
