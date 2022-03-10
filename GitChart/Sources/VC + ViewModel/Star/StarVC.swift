eb1c80e64c7c00e8afeb65b77cedcc4488ad0992
김대희 <0824dh@naver.com>
2022-03-10 15:43:23 +0900

    Add :: StarVC add mainTableView

--- GitChart/Sources/VC + ViewModel/Star/StarVC.swift
+++ GitChart/Sources/VC + ViewModel/Star/StarVC.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class StarVC : BaseViewController {
     
+    let mainTableView = UITableView().then {
+        $0.backgroundColor = .clear
+        $0.separatorStyle = .none
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.appColor(.labelColor), .font : UIFont.roundedFont(ofSize: 20, weight: .semibold)]
         self.navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.appColor(.labelColor), .font : UIFont.roundedFont(ofSize: 34, weight: .bold)]
@@ -17,5 +22,13 @@ class StarVC : BaseViewController {
     override func configureUI() {
         self.title = "Star"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.sizeToFit()
+        view.addSubview(mainTableView)
+    }
+    
+    override func setupConstraints() {
+        mainTableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
