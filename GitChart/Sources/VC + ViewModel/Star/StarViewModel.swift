a905124019bdf3c492a3f7ef5358cf6d53e80224
김대희 <0824dh@naver.com>
2022-03-10 19:10:44 +0900

    Add :: StarViewModwl.swift

---
+++ GitChart/Sources/VC + ViewModel/Star/StarViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  StarViewModel.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/03/10.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+class StarViewModwl : ViewModel {
+    
+    let disposeBag = DisposeBag()
+    let getUserStarList = PublishRelay<[Starred]>()
+    let apiStatus = PublishRelay<Bool>()
+    
+    struct input {}
+    
+    struct output {
+        let getUserStarList : PublishRelay<[Starred]>
+        let apiStatus : PublishRelay<Bool>
+    }
+    
+    func trans(_ input: input) -> output {
+        API.getUserStarred("kimdaehee0824").request()
+            .subscribe { event in
+                
+                switch event {
+                case .success(let response):
+                    print(response.data)
+                    guard let data = try? JSONDecoder().decode([Starred].self, from: response.data) else {
+                        self.apiStatus.accept(false)
+                        return
+                    }
+                    self.getUserStarList.accept(data)
+                    self.apiStatus.accept(true)
+                case .failure(let error):
+                    print("😔 error : \(error)")
+                    self.apiStatus.accept(false)
+                }
+                
+            }.disposed(by: disposeBag)
+        
+        return output(getUserStarList: getUserStarList, apiStatus: apiStatus)
+    }
+    
+    
+}
