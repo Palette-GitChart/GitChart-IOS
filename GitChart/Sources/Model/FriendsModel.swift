58b9ebdeec85e557c68389fb6feec63ce057ff13
김대희 <0824dh@naver.com>
2022-03-14 08:14:36 +0900

    Add :: FriendsModel.swift

---
+++ GitChart/Sources/Model/FriendsModel.swift
@@ -0,0 +1,14 @@
+//
+//  FriendsModel.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/03/14.
+//
+
+import Foundation
+
+struct Friends : Codable {
+    let login : String
+    let avatar_url : String
+    let html_url : String
+}
