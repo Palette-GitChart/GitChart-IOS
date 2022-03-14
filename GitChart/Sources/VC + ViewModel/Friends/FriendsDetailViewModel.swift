8d9ca2f05ddb70c08c3dcdfee67b742b3222c797
김대희 <0824dh@naver.com>
2022-03-14 12:52:33 +0900

    Add :: FriendsDetailViewModel transfom

--- GitChart/Sources/VC + ViewModel/Friends/FriendsDetailViewModel.swift
+++ GitChart/Sources/VC + ViewModel/Friends/FriendsDetailViewModel.swift
@@ -12,6 +12,8 @@ import RxRelay
 class FriendsDetailViewModel : ViewModel {
 
     let disposeBag = DisposeBag()
+    let getDayCommit = PublishRelay<String>()
+    let getYearCommit = PublishRelay<String>()
     let apiStatus = PublishRelay<Bool>()
     
     struct input {
@@ -19,11 +21,39 @@ class FriendsDetailViewModel : ViewModel {
     }
     
     struct output {
+        let getDayCommit : PublishRelay<String>
+        let getYearCommit : PublishRelay<String>
         let apiStatus : PublishRelay<Bool>
     }
     
     func trans(_ input: input) -> output {
-        return output(apiStatus: apiStatus)
+        
+        API.dayCommit(input.username).request()
+            .subscribe { event in
+                switch event {
+                case .success(let response):
+                    let data = String(data: response.data, encoding: .utf8)
+                    self.getDayCommit.accept(data!)
+                    self.apiStatus.accept(true)
+                case .failure(let error):
+                    print("😔 error : \(error)")
+                    self.apiStatus.accept(false)
+                }
+            }.disposed(by: disposeBag)
+        
+        API.yearCommit(input.username).request()
+            .subscribe { event in
+                switch event {
+                case .success(let response):
+                    let data = String(data: response.data, encoding: .utf8)
+                    self.getYearCommit.accept(data!)
+                    self.apiStatus.accept(true)
+                case .failure(let error):
+                    print("😔 error : \(error)")
+                    self.apiStatus.accept(false)
+                }
+            }.disposed(by: disposeBag)
+        
+        return output(getDayCommit: getDayCommit, getYearCommit: getYearCommit, apiStatus: apiStatus)
     }
-
 }
