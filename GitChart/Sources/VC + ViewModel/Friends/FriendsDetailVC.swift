b3597a2833399bb5f81d166c7ae78814f48d04ba
김대희 <0824dh@naver.com>
2022-03-14 12:12:27 +0900

    Add :: year, day view와 그 안에 라벨들

--- GitChart/Sources/VC + ViewModel/Friends/FriendsDetailVC.swift
+++ GitChart/Sources/VC + ViewModel/Friends/FriendsDetailVC.swift
@@ -23,7 +23,7 @@ class FriendsDetailVC : BaseViewController {
         $0.font = .roundedFont(ofSize: 25, weight: .semibold)
         $0.textAlignment = .center
     }
-        
+    
     let visitWebButton = UIButton().then {
         $0.setTitle("Web에서 보기", for: .normal)
         $0.backgroundColor = UIColor(rgb: 0x67C58E).withAlphaComponent(0.9)
@@ -31,7 +31,7 @@ class FriendsDetailVC : BaseViewController {
         $0.titleLabel?.font = .notoFont(size: .Regular, ofSize: 22)
         $0.layer.cornerRadius = 15
     }
-
+    
     let followerLabel = UILabel().then {
         $0.textAlignment = .right
     }
@@ -39,6 +39,41 @@ class FriendsDetailVC : BaseViewController {
         $0.textAlignment = .left
     }
     
+    let dayCommitView = UIView()
+    let yearCommitView = UIView()
+    
+    let dayCommitLabel = UILabel()
+    let yearCommitLabel = UILabel()
+    
+    let dayCommitCountLabel = UILabel()
+    let yearCommitCountLabel = UILabel()
+    
+    func makeCommitView() {
+        let commitView = [dayCommitView, yearCommitView]
+        let commitLabel = [dayCommitLabel, yearCommitLabel]
+        commitView.forEach {
+            $0.backgroundColor = .appColor(.cellColor)
+            $0.layer.cornerRadius = 20
+        }
+        commitLabel.forEach {
+            $0.font = .roundedFont(ofSize: 20, weight: .semibold)
+            $0.textColor = .appColor(.labelColor)
+        }
+    }
+    
+    func makeCountText() {
+        [dayCommitCountLabel, yearCommitCountLabel].forEach {
+            let str = $0.text ?? ""
+            
+            let attributedStr = NSMutableAttributedString(string: $0.text ?? "")
+            let range = (str as NSString).range(of: "개")
+            attributedStr.addAttribute(.font, value: UIFont.notoFont(size: .Regular, ofSize: 25), range: range)
+            attributedStr.addAttribute(.foregroundColor, value: UIColor.appColor(.labelColor), range: range)
+            
+            $0.font = .roundedFont(ofSize: 50, weight: .medium)
+            $0.attributedText = attributedStr
+        }
+    }
     override func configureUI() {
         navigationItem.largeTitleDisplayMode = .never
         self.navigationController?.navigationBar.tintColor = .appColor(.mainColor)
