922f3ae9d96b5336cfe554f2d5bb8806742de20e
김대희 <0824dh@naver.com>
2022-03-15 23:15:55 +0900

    Add :: TrandCommitTableViewCell 엔티티들 생성 & addSubview

--- GitChart/Sources/VC + ViewModel/Trand/TrandCommitTableViewCell.swift
+++ GitChart/Sources/VC + ViewModel/Trand/TrandCommitTableViewCell.swift
@@ -9,17 +9,47 @@ import UIKit
 
 class TrandCommitTableViewCell: BaseTableViewCell {
     
-    let mainView = UIView().then {
-        $0.backgroundColor = .appColor(.cellColor)
-        $0.layer.cornerRadius = 20
-    }
+    let mainView = UIView()
     
-    var titleLabel = UILabel().then {
-        $0.font = .roundedFont(ofSize: 20, weight: .semibold)
-        $0.textColor = .appColor(.labelColor)
+    let mounthCommitView = UIView()
+    let yearCommitView = UIView()
+    
+    let mounthCommitLabel = UILabel()
+    let yearComitLabel = UILabel()
+    
+    let mountCommitCountLabel = UILabel()
+    let yearCommitCountLabel = UILabel()
+    
+    func makeView() {
+        
+        mainView.backgroundColor = .clear
+        
+        [mounthCommitView, yearCommitView].forEach {
+            $0.backgroundColor = .appColor(.cellColor)
+            $0.layer.cornerRadius = 20
+            
+        }
+        
+        [mounthCommitLabel, yearComitLabel].forEach {
+            $0.font = .roundedFont(ofSize: 20, weight: .semibold)
+            $0.textColor = .appColor(.labelColor)
+        }
+        
     }
-
+        
     override func configureUI() {
         contentView.backgroundColor = .appColor(.backgroundColor)
+        contentView.addSubview(mainView)
+        
+        [mounthCommitView, yearCommitView].forEach {
+            mainView.addSubview($0)
+        }
+        [mounthCommitLabel, mountCommitCountLabel].forEach {
+            mounthCommitView.addSubview($0)
+        }
+        [yearComitLabel, yearCommitCountLabel].forEach {
+            yearCommitView.addSubview($0)
+        }
+        makeView()
     }
 }
