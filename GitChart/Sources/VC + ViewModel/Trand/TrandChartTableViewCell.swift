218bb253e617029abd184e7edf496cb4113d858e
김대희 <0824dh@naver.com>
2022-03-15 19:52:18 +0900

    Add :: TrandChartTableViewCell.swift & TrandCommitTableViewCell.swift

---
+++ GitChart/Sources/VC + ViewModel/Trand/TrandChartTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TrandChartTableViewCell.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/03/15.
+//
+
+import UIKit
+
+class TrandChartTableViewCell: BaseTableViewCell {
+    override func configureUI() {
+        contentView.backgroundColor = .appColor(.backgroundColor)
+    }
+}
---
+++ GitChart/Sources/VC + ViewModel/Trand/TrandCommitTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TrandCommitTableViewCell.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/03/15.
+//
+
+import UIKit
+
+class TrandCommitTableViewCell: BaseTableViewCell {
+    override func configureUI() {
+        contentView.backgroundColor = .appColor(.backgroundColor)
+    }
+}
