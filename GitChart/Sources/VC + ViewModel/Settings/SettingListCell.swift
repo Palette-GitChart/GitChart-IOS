a65c461cc84f8acd3495f42dc190187e601068fb
김대희 <0824dh@naver.com>
2022-03-21 13:19:15 +0900

    Add :: SettingListCell.swift

---
+++ GitChart/Sources/VC + ViewModel/Settings/SettingListCell.swift
@@ -0,0 +1,14 @@
+//
+//  SettingListCell.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/03/21.
+//
+
+import Foundation
+
+class SettingListCell : BaseTableViewCell {
+    override func configureUI() {
+        contentView.backgroundColor = .appColor(.backgroundColor)
+    }
+}
