88c1a074aed8b1b8034697219197479f15253371
김대희 <0824dh@naver.com>
2022-03-21 19:18:12 +0900

    Modify :: RxCocoa 제거 후 deldgate

--- GitChart/Sources/VC + ViewModel/Settings/SettingVC.swift
+++ GitChart/Sources/VC + ViewModel/Settings/SettingVC.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import RxSwift
 
 class SettingVC : BaseViewController {
     
+    let settingTitleArray = ["Github 아이디 추가 변경", "Commit Trand 기간 변경하기", "일일 커밋 목표 수정하기", "개발자 소개", "오픈소스", "문의하기"]
+    
     let mainTableView = UITableView().then {
+        $0.register(SettingListCell.self, forCellReuseIdentifier: "settingListCell")
         $0.backgroundColor = .clear
         $0.separatorStyle = .none
     }
@@ -21,9 +25,34 @@ class SettingVC : BaseViewController {
     override func configureUI() {
         self.title = "Setting"
         view.addSubview(mainTableView)
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
     }
+    
     
     override func setupConstraints() {
         mainTableView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+}
+
+extension SettingVC : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        switch row {
+        case 0:
+            return SettingHeaderCell()
+        case 1, 2, 3, 4, 5, 6:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "settingListCell") as! SettingListCell
+            cell.textLabbel.text = settingTitleArray[row-1]
+            return cell
+        case 7:
+            return VersionCell()
+        default:
+            return UITableViewCell()
+        }
     }
 }
