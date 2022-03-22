e1f64e3130f85608552c10c3f009d51845089598
김대희 <0824dh@naver.com>
2022-03-22 16:54:19 +0900

    Add :: AddUserNameVC.swift

---
+++ GitChart/Sources/VC + ViewModel/Settings/AddUserNameVC.swift
@@ -0,0 +1,17 @@
+//
+//  AddUserNameVC.swift
+//  GitChart
+//
+//  Created by 김대희 on 2022/03/22.
+//
+
+import UIKit
+
+final class AddUserNameVC : BaseViewController {
+    
+    private let titleLabel = UILabel()
+    private let usernameTextField = UITextField()
+    
+    
+    
+}
