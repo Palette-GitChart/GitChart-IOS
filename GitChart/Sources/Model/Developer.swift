f9b012f16d89ee90728bff3c768a67347a02b31c
김대희 <0824dh@naver.com>
2022-03-24 18:10:01 +0900

    Add :: Developer struct url

--- GitChart/Sources/Model/Developer.swift
+++ GitChart/Sources/Model/Developer.swift
@@ -12,10 +12,12 @@ struct Developer {
     let description : [String]
     let image : [String]
     let portfolio : [String]
+    let url : [String]
     init () {
         self.mame = ["Daehee Kim", "Yongjin Lee", "Seokyeon Jang", "Sangwoon Lee"]
         self.image =  ["profiedaeheeKim", "profieYongjinLee", "profileseokyounJang", "profilesangwoonLee"]
         self.description = ["IOS Developer & Design", "Android Developer", "Design", "Backend Developer"]
         self.portfolio = ["github", "github", "behance", "github"]
+        self.url = ["https://github.com/kimdaehee0824", "https://github.com/Leesin0222", "https://www.behance.net/ffe58c70", "https://github.com/biscrab"]
     }
 }
