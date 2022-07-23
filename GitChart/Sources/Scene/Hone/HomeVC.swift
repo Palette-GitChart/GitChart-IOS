d2ac6f10d28bff5066af91fcd9ef265983066957
Daehee Kim <0824dh@naver.com>
2022-07-23 23:07:21 +0900

    Modity :: 뭔가 모르겠는 파일 변경

--- GitChart/Sources/Scene/Hone/HomeVC.swift
+++ GitChart/Sources/Scene/Hone/HomeVC.swift
@@ -116,7 +116,7 @@ class HomeVC : BaseViewController {
         }
         commitTrandCountLabel2.textAlignment = .right
     }
-
+    
     
     let trandChart = LineChartView()
     func cellViewMake() {
@@ -149,7 +149,7 @@ class HomeVC : BaseViewController {
         refreshControl.tintColor = .appColor(.labelColor)
         refreshControl.endRefreshing() // 초기화 - refresh 종료
         scrollView.refreshControl = refreshControl
-
+        
         commitLabel1.text = "Today Commit"
         commitLabel2.text = "week commit"
         
